354424160d1a753da65994ba5a916dd80d614b0c
ValeriyKryatov <Kwaler@mail.ru>
2023-09-22 22:34:23 +0300

    Coursework2ExamQuestionGenerator. First commit

---
+++ src/main/java/Coursework2ExamQuestionGenerator/Coursework2ExamQuestionGeneratorApplication.java
@@ -0,0 +1,12 @@
+package Coursework2ExamQuestionGenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Coursework2ExamQuestionGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Coursework2ExamQuestionGeneratorApplication.class, args);
+	}
+}
